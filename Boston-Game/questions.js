ab344c08ddee538f96e69636b6297d85efa1ad53
Faraz Khalid <ifarazkhalid@gmail.com>
2018-05-24 15:53:19 -0500

    Another Alexa Skill
    Boston Quiz Game asks you trivia questions about Boston

---
+++ Boston-Game/questions.js
@@ -0,0 +1,139 @@
+module.exports = {
+
+  QUESTIONS_EN_US: [
+    {
+      'What is the nickname given to the city of Boston?': [
+        'Bean town',
+        'Big Easy',
+        'Beehive of Industry',
+        'Big D',
+        'Ped Town',
+        'College Town',
+      ],
+    },
+    {
+      'What are the names of the two major daily newspapers in Boston, MA?': [
+        'Globe and Daily News',
+        'Times and Lantern',
+        'Learner and Times',
+        'Globe and Herald',
+        'NY Times and Lantern',
+      ],
+    },
+    {
+      'What river separates Boston and Cambridge?': [
+        'Charles River',
+        'River Thames',
+        'Muddy River',
+        'River Boston and Cambridge',
+        'There is not a river that seperates them'
+      ],
+    },
+    {
+      'Paul Reveres ride happened on which date?': [
+        'April 18, 1775',
+        'June 3, 1775',
+        'Midnight of some day',
+        'August 4th, 1775',
+      ],
+    },
+    {
+      'From which Boston museum were $200 million worth of paintings stolen in 1990?': [
+        'Isabella Stewart Gardner Museum',
+        'Mary Baker Eddy House',
+        'MIT',
+        'Beantown Museum',
+      ],
+    },
+    {
+      'Which of these is not a stop on the Orange Line of Boston\'s "T"?': [
+        'Airport',
+        'North Station',
+        'Wellington',
+        'China Town',
+      ],
+    },
+    {
+      'Where in Boston would you ride the "Swan Boats"?': [
+        'Public Garden',
+        'Frog Pond',
+        'Charles River',
+        'Boston Commons',
+      ],
+    },
+    {
+      'When Boston hosted the NCAA division I mens ice hockey Frozen Four, what school won the national title?': [
+        'Michigan',
+        'Hempshire',
+        'Ohio State',
+        'Boston College',
+      ],
+    },
+    {
+      'Who won the first Boston Marathon on April 19, 1897?': [
+        'John J McDermott',
+        'John Kelly',
+        'John Kelly 1',
+        'King Kelly',
+      ],
+    },
+    {
+      'Which year was Paul Revere born?': [
+        '1734',
+        '1750',
+        '1760',
+        '1736',
+      ],
+    },
+    {
+      'What was the name of the horse that Paul Revere rode on the night of his famous ride?': [
+        'Brown Beauty',
+        'Black Beauty',
+        'Boston Beauty',
+        'Rampart',
+        'Schenzerdane'
+      ],
+    },
+    {
+      'Which of the following is a nickname given to the city of Boston?': [
+        'Athens of America',
+        'Birthplace of Baseball',
+        'Best City of America',
+        'Most Historic City',
+      ],
+    },
+    {
+      'Where was the Boston Museum of Fine Arts located before opening on Huntington Avenue?': [
+        'Copley Square',
+        'Cambridge',
+        'East Boston Waterfront',
+        'South Station',
+      ],
+    },
+    {
+      'What was the first production at Boston\'s Colonial Theatre?': [
+        'Ben Hur',
+        'Fiddler On The Roof',
+        'Alie in Wonderland',
+        'The King and I',
+
+      ],
+    },
+    {
+      'In 1750, Boston Puritans prohibited what activity?': [
+        'Performence of Plays',
+        'Spitting',
+        'Walking',
+        'Jaywalking',
+      ],
+    },
+    {
+      'What is the six-mile chain of parks running from Fenway to the Arnold Arboretum known as in Boston?': [
+        'Emerald Necklace',
+        'Emerald Line',
+        'Emerald Park',
+        'Emerald Isle',
+      ],
+    }
+  ]
+};
